588d070c3665bbf193cd73e776aabd07cb74e10b
danilopinhomoreira <146568955+danilopinhomoreira@users.noreply.github.com>
2024-06-23 23:51:06 -0300

    Update Cardapio.cs

--- codigo/Cardapio.cs
+++ codigo/Cardapio.cs
@@ -1,30 +1,58 @@
 ﻿using System.Collections.Generic;
 
-// Classe para representar o cardápio do café
+// Classe para representar o cardápio
 public class Cardapio
 {
     private List<IProduto> produtos;
+    private int ultimoCodigoConsultado; // Atributo para armazenar o último código consultado
 
     public Cardapio()
     {
-        produtos = new List<IProduto>();
-    }
-
-    // Método para adicionar produto ao cardápio
-    public void AdicionarProduto(IProduto produto)
+        produtos = new List<IProduto>
     {
-        produtos.Add(produto);
+        new Comida("Moqueca de Palmito", 32, 1),
+        new Comida("Falafel Assado", 20, 2),
+        new Comida("Salada Primavera com Macarrão Konjac", 25, 3),
+        new Comida("Escondidinho de Inhame", 18, 4),
+        new Comida("Strogonoff de Cogumelos", 35, 5),
+        new Comida("Caçarola de legumes", 22, 6),
+        new Bebida("Água", 3, 7),
+        new Bebida("Copo de suco", 7, 8),
+        new Bebida("Refrigerante orgânico", 7, 9),
+        new Bebida("Cerveja vegana", 9, 10),
+        new Bebida("Taça de vinho vegano", 18, 11)
+    };
     }
 
-    // Método para listar todos os produtos do cardápio
     public List<IProduto> ListarProdutos()
     {
         return produtos;
     }
 
-    // Método para buscar um produto pelo nome
-    public IProduto BuscarProduto(string nome)
+    public List<Comida> ListarComidas()
     {
-        return produtos.Find(p => p.Nome == nome);
+        return produtos.OfType<Comida>().ToList();
+    }
+
+    public List<Bebida> ListarBebidas()
+    {
+        return produtos.OfType<Bebida>().ToList();
+    }
+
+    private void DefinirUltimoCodigoConsultado(int codigo)
+    {
+        ultimoCodigoConsultado = codigo;
+    }
+
+    public IProduto BuscarProdutoPorCodigo()
+    {
+        foreach (var produto in produtos)
+        {
+            if (produto.Codigo == ultimoCodigoConsultado)
+            {
+                return produto;
+            }
+        }
+        return null;
     }
 }

41e89f4831ed5f38f9fdaf5da095ea2309c9850d
danilopinhomoreira <146568955+danilopinhomoreira@users.noreply.github.com>
2024-06-23 23:55:33 -0300

    Update Bebida.cs
    Adição do atributo código.

--- codigo/Bebida.cs
+++ codigo/Bebida.cs
@@ -1,5 +1,7 @@
 ﻿// Classe para representar bebidas
-public class Bebida : Produto
-{
-    // Outros atributos específicos de bebida podem ser adicionados aqui, se necessário
-}
+ public class Bebida : Produto
+ {
+     public Bebida(string nome, double preco, int codigo) : base(nome, preco, codigo)
+     {
+     }
+ }

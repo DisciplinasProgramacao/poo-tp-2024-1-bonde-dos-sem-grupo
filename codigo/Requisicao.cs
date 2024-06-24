7417f32cc78cd925efe1f1dab1069f9e9dea79f9
danilopinhomoreira <146568955+danilopinhomoreira@users.noreply.github.com>
2024-06-23 23:46:05 -0300

    Atualização Requisicao.cs
    Atualização da classe Requisição conforme requisitos da Sprint 2.

--- codigo/Requisicao.cs
+++ codigo/Requisicao.cs
@@ -2,32 +2,47 @@ using System;
 
 public class Requisicao
 {
-    Cliente cliente;
-    int numClientes;
-    Mesa mesa;
-    DateTime dataReq;
-    DateTime horaEntrada;
-    DateTime horaSaida;
+    public int Id { get; private set; }
+    public Cliente Cliente { get; private set; }
+    public int NumClientes { get; private set; }
+    public Mesa Mesa { get; set; }
+    public DateTime DataReq { get; private set; }
+    public DateTime HoraEntrada { get; private set; }
+    public DateTime HoraSaida { get; private set; }
+    private List<IProduto> produtosConsumidos;
 
-    public void criarRequisicao(Cliente cliente, int numClientes, Mesa mesa)
+    public Requisicao(Cliente cliente, int numClientes)
     {
-        this.cliente = cliente;
-        this.numClientes = numClientes;
-        this.mesa = mesa;
-        this.dataReq = DateTime.Now;
-        this.horaEntrada = DateTime.Now;
-        // Pode ser aprimorado
+        Cliente = cliente;
+        NumClientes = numClientes;
+        DataReq = DateTime.Now;
+        HoraEntrada = DateTime.Now;
+        produtosConsumidos = new List<IProduto>();
     }
 
-    public void encerrarRequisicao()
+    public void AdicionarProduto(IProduto produto)
     {
-        this.horaSaida = DateTime.Now;
-        TimeSpan duracao = horaSaida - horaEntrada;
-        Console.WriteLine($"Requisicao encerrada para {cliente.Nome} na mesa {mesa.Numero}.");
-        Console.WriteLine($"Numero de clientes: {numClientes}");
-        Console.WriteLine($"Hora de entrada: {horaEntrada}");
-        Console.WriteLine($"Hora de saida: {horaSaida}");
-        Console.WriteLine($"Duracaoo da requisicao: {duracao}");
-        // Pode ser aprimorado
+        produtosConsumidos.Add(produto);
+        Console.WriteLine($"Produto {produto.Nome} adicionado à requisição.");
+    }
+
+    public void EncerrarRequisicao()
+    {
+        HoraSaida = DateTime.Now;
+        Console.WriteLine($"Requisição encerrada.");
+    }
+
+    public void MostrarConta()
+    {
+        decimal total = 0;
+        Console.WriteLine($"Conta da Requisição:");
+
+        foreach (var produto in produtosConsumidos)
+        {
+            Console.WriteLine($"{produto.Nome} - R$ {produto.Preco}");
+            total += (decimal)produto.Preco; // Conversão explícita para decimal
+        }
+
+        Console.WriteLine($"Total: R$ {total}");
     }
 }

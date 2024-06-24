a9790acb2f9c69a24b2b9e26c15a8a22b108840e
danilopinhomoreira <146568955+danilopinhomoreira@users.noreply.github.com>
2024-06-23 23:56:08 -0300

    Update Comida.cs
    Adição do atributo código.

--- codigo/Comida.cs
+++ codigo/Comida.cs
@@ -1,5 +1,7 @@
 ﻿// Classe para representar comidas
 public class Comida : Produto
 {
-    // Outros atributos específicos de comida podem ser adicionados aqui, se necessário
+    public Comida(string nome, double preco, int codigo) : base(nome, preco, codigo)
+    {
+    }
 }

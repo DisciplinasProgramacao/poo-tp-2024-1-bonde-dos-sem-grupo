432f0d02a5d7bc1207ee4c1dcdd6f0ed94508983
danilopinhomoreira <146568955+danilopinhomoreira@users.noreply.github.com>
2024-06-27 19:31:49 -0300

    Update Requisicao.cs
    Retorno para a versão da Sprint 2.

--- codigo/Requisicao.cs
+++ codigo/Requisicao.cs
@@ -1,63 +1,82 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Requisicao
+namespace Danilo_sFood
 {
-    private int id;
-    private Cliente cliente;
-    private int numClientes;
-    private List<IProduto> produtos;
-    private Mesa mesa;
-    private static int idCounter = 1;
-
-    public Requisicao(Cliente cliente, int numClientes)
+    public class Requisicao
     {
-        this.id = idCounter++;
-        this.cliente = cliente;
-        this.numClientes = numClientes;
-        produtos = new List<IProduto>();
-    }
+        private int id;
+        private Cliente cliente;
+        private int numClientes;
+        private List<IProduto> produtos;
+        private Mesa mesa;
+        private int posicaoListaEspera;
+        private static int idCounter = 1;
 
-    public int GetNumClientes()
-    {
-        return numClientes;
-    }
-
-    public Cliente GetCliente()
-    {
-        return cliente;
-    }
-
-    public Mesa GetMesa()
-    {
-        return mesa;
-    }
-
-    public void SetMesa(Mesa mesa)
-    {
-        this.mesa = mesa;
-    }
-
-    public void AdicionarProduto(IProduto produto)
-    {
-        produtos.Add(produto);
-        Console.WriteLine($"Produto {produto.GetNome()} adicionado à requisição.");
-    }
-
-    public void EncerrarRequisicao()
-    {
-        Console.WriteLine($"Requisição {id} encerrada.");
-    }
-
-    public void MostrarConta()
-    {
-        Console.WriteLine("\nConta:");
-
-        foreach (var produto in produtos)
+        public Requisicao(Cliente cliente, int numClientes)
         {
-            Console.WriteLine($"{produto.GetNome()} - R$ {produto.GetPreco()}");
+            this.id = idCounter++;
+            this.cliente = cliente;
+            this.numClientes = numClientes;
+            produtos = new List<IProduto>();
         }
 
-        double total = produtos.Sum(p => p.GetPreco());
-        Console.WriteLine($"\nTotal: R$ {total}");
+        public int GetNumClientes()
+        {
+            return numClientes;
+        }
+
+        public Cliente GetCliente()
+        {
+            return cliente;
+        }
+
+        public Mesa GetMesa()
+        {
+            return mesa;
+        }
+
+        public void SetMesa(Mesa mesa)
+        {
+            this.mesa = mesa;
+        }
+
+        public int GetPosicaoListaEspera()
+        {
+            return posicaoListaEspera;
+        }
+
+        public void SetPosicaoListaEspera(int posicao)
+        {
+            this.posicaoListaEspera = posicao;
+        }
+
+        public void AdicionarProduto(IProduto produto)
+        {
+            produtos.Add(produto);
+        }
+
+        public double CalcularTotal()
+        {
+            return produtos.Sum(p => p.GetPreco());
+        }
+
+        public string MostrarConta()
+        {
+            StringBuilder conta = new StringBuilder();
+
+            foreach (var produto in produtos)
+            {
+                conta.AppendLine($"{produto.GetNome()} - R$ {produto.GetPreco()}");
+            }
+
+            conta.AppendLine($"Total: R$ {CalcularTotal()}");
+
+            return conta.ToString();
+        }
     }
+
 }

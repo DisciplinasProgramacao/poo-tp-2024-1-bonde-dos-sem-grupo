182635be1bba2f077cbad422193e655db7185f7e
danilopinhomoreira <146568955+danilopinhomoreira@users.noreply.github.com>
2024-06-27 19:34:30 -0300

    Update Restaurante.cs
    Retorno para a versão da Sprint 2.

--- codigo/Restaurante.cs
+++ codigo/Restaurante.cs
@@ -1,136 +1,125 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
-public class Restaurante
+namespace Danilo_sFood
 {
-    private List<Cliente> clientes;
-    private List<Mesa> mesas;
-    private List<Requisicao> requisicoesEmAndamento; // Lista de requisições em andamento
-    private List<Requisicao> requisicoesEmEspera; // Lista de requisições em espera
-    private Cardapio cardapio;
-
-    public Restaurante()
+    public class Restaurante
     {
-        clientes = new List<Cliente>();
-        mesas = new List<Mesa>();
-        requisicoesEmAndamento = new List<Requisicao>(); // Inicialização da lista de requisições em andamento
-        requisicoesEmEspera = new List<Requisicao>(); // Inicialização da lista de requisições em espera
-        cardapio = new Cardapio(); // Inicialização do Cardápio
-    }
+        private List<Cliente> clientes;
+        private List<Mesa> mesas;
+        private List<Requisicao> requisicoesEmAndamento;
+        private List<Requisicao> requisicoesEmEspera;
+        private Cardapio cardapio;
 
-    public Cliente BuscarClientePorCpf(string cpf)
-    {
-        return clientes.Find(c => c.GetCpf() == cpf);
-    }
-
-    public Cliente CadastrarNovoCliente(string nome, string cpf)
-    {
-        Cliente novoCliente = new Cliente(nome, cpf);
-        clientes.Add(novoCliente);
-        return novoCliente;
-    }
-
-    public void AdicionarMesa(Mesa mesa)
-    {
-        mesas.Add(mesa);
-        Console.WriteLine($"Mesa {mesa.GetIdMesa()} com {mesa.GetLugaresMesa()} lugares adicionada.");
-    }
-
-    public Requisicao CriarRequisicao(Cliente cliente, int numPessoas)
-    {
-        var requisicao = new Requisicao(cliente, numPessoas);
-        Console.WriteLine($"Requisição criada para o cliente {cliente.GetNome()}.");
-        string resultadoMesa = DefinirMesa(requisicao); // Aloca uma mesa para a nova requisição
-        Console.WriteLine(resultadoMesa);
-
-        // Adiciona a requisição à lista de requisições em andamento
-        requisicoesEmAndamento.Add(requisicao);
-
-        return requisicao;
-    }
-
-    public string DefinirMesa(Requisicao requisicao)
-    {
-        // Ordena as mesas por capacidade em ordem crescente
-        var mesasDisponiveis = mesas.Where(m => m.VerificaDisponibilidade())
-                                     .OrderBy(m => m.GetLugaresMesa())
-                                     .ToList();
-
-        // Busca a primeira mesa com capacidade maior ou igual ao número de pessoas na requisição
-        var mesaEscolhida = mesasDisponiveis.FirstOrDefault(m => m.GetLugaresMesa() >= requisicao.GetNumClientes());
-
-        if (mesaEscolhida != null)
+        public Restaurante()
         {
-            mesaEscolhida.SetStatus(false);
-            requisicao.SetMesa(mesaEscolhida);
-            Console.WriteLine($"Mesa {mesaEscolhida.GetIdMesa()} definida para a requisição de {requisicao.GetCliente().GetNome()}.");
-            return $"Mesa {mesaEscolhida.GetIdMesa()} definida para a requisição de {requisicao.GetCliente().GetNome()}.";
+            clientes = new List<Cliente>();
+            mesas = new List<Mesa>();
+            requisicoesEmAndamento = new List<Requisicao>();
+            requisicoesEmEspera = new List<Requisicao>();
+            cardapio = new Cardapio();
         }
-        else
-        {
-            AdicionarNaListaEspera(requisicao);
-            int posicao = requisicoesEmEspera.IndexOf(requisicao) + 1;
-            Console.WriteLine($"Mesa não disponível. Requisição adicionada à lista de espera na posição {posicao}.");
-            return $"Mesa não disponível. Requisição adicionada à lista de espera na posição {posicao}.";
-        }
-    }
 
-    public string LiberarMesa(Requisicao requisicao)
-    {
-        if (requisicao.GetMesa() != null)
+        public Cliente BuscarClientePorCpf(string cpf)
         {
-            requisicao.GetMesa().SetStatus(true);
-            Console.WriteLine($"Mesa {requisicao.GetMesa().GetIdMesa()} liberada.");
-            return $"Mesa {requisicao.GetMesa().GetIdMesa()} liberada.";
+            return clientes.Find(c => c.GetCpf() == cpf);
         }
-        else
-        {
-            Console.WriteLine("Mesa não encontrada.");
-            return "Mesa não encontrada.";
-        }
-    }
 
-    public void AdicionarNaListaEspera(Requisicao requisicao)
-    {
-        requisicoesEmEspera.Add(requisicao);
-    }
-
-    public void AtenderRequisicao()
-    {
-        if (requisicoesEmEspera.Count > 0)
+        public Cliente CadastrarNovoCliente(string nome, string cpf)
         {
-            var requisicaoMaisAntiga = requisicoesEmEspera[0];
-            requisicoesEmEspera.RemoveAt(0);
-            Console.WriteLine($"Requisição de {requisicaoMaisAntiga.GetNumClientes()} clientes atendida.");
+            Cliente novoCliente = new Cliente(nome, cpf);
+            clientes.Add(novoCliente);
+            return novoCliente;
         }
-        else
-        {
-            Console.WriteLine("Não há requisições na lista de espera.");
-        }
-    }
 
-    public string FecharConta(int numeroMesa)
-    {
-        // Busca a requisição associada à mesa informada na lista de requisições em andamento
-        var requisicaoParaFechar = requisicoesEmAndamento.FirstOrDefault(r => r.GetMesa().GetIdMesa() == numeroMesa);
-
-        if (requisicaoParaFechar != null)
+        public void AdicionarMesa(Mesa mesa)
         {
-            requisicaoParaFechar.MostrarConta();
-            // Adiciona a requisição à lista de requisições encerradas
-            requisicoesEmEspera.Add(requisicaoParaFechar);
-            LiberarMesa(requisicaoParaFechar); // Liberar a mesa associada à requisição
-                                               // Remove a requisição da lista de requisições em andamento
-            requisicoesEmAndamento.Remove(requisicaoParaFechar);
-            return "Conta fechada com sucesso.";
+            mesas.Add(mesa);
         }
-        else
+
+        public Requisicao CriarRequisicao(Cliente cliente, int numPessoas)
         {
-            return "Não foi encontrada nenhuma requisição em andamento para a mesa informada.";
+            var requisicao = new Requisicao(cliente, numPessoas);
+            var resultadoMesa = DefinirMesa(requisicao);
+            requisicao.SetPosicaoListaEspera(resultadoMesa.Item2);
+            requisicoesEmAndamento.Add(requisicao);
+            return requisicao;
+        }
+
+        public Tuple<Mesa, int> DefinirMesa(Requisicao requisicao)
+        {
+            var mesasDisponiveis = mesas.Where(m => m.VerificaDisponibilidade())
+                                         .OrderBy(m => m.GetLugaresMesa())
+                                         .ToList();
+
+            var mesaEscolhida = mesasDisponiveis.FirstOrDefault(m => m.GetLugaresMesa() >= requisicao.GetNumClientes());
+
+            if (mesaEscolhida != null)
+            {
+                mesaEscolhida.SetStatus(false);
+                requisicao.SetMesa(mesaEscolhida);
+                return Tuple.Create(mesaEscolhida, -1);
+            }
+            else
+            {
+                AdicionarNaListaEspera(requisicao);
+                int posicao = requisicoesEmEspera.IndexOf(requisicao) + 1;
+                return Tuple.Create<Mesa, int>(null, posicao);
+            }
+        }
+
+        public void LiberarMesa(Requisicao requisicao)
+        {
+            if (requisicao.GetMesa() != null)
+            {
+                requisicao.GetMesa().SetStatus(true);
+            }
+        }
+
+        public void AdicionarNaListaEspera(Requisicao requisicao)
+        {
+            requisicoesEmEspera.Add(requisicao);
+        }
+
+        public void AtenderRequisicao()
+        {
+            if (requisicoesEmEspera.Count > 0)
+            {
+                var requisicaoMaisAntiga = requisicoesEmEspera[0];
+                requisicoesEmEspera.RemoveAt(0);
+                DefinirMesa(requisicaoMaisAntiga);
+            }
+        }
+
+        public string FecharConta(int numeroMesa)
+        {
+            var requisicaoParaFechar = requisicoesEmAndamento.FirstOrDefault(r => r.GetMesa().GetIdMesa() == numeroMesa);
+
+            if (requisicaoParaFechar != null)
+            {
+                string conta = requisicaoParaFechar.MostrarConta();
+                requisicoesEmEspera.Add(requisicaoParaFechar);
+                LiberarMesa(requisicaoParaFechar);
+                requisicoesEmAndamento.Remove(requisicaoParaFechar);
+                return conta;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Cardapio GetCardapio()
+        {
+            return cardapio;
         }
     }
 
-    public Cardapio GetCardapio()
-    {
-        return cardapio;
-    }
+
+
+
 }

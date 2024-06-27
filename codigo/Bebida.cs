859c05e064bb18f0829ae1aef652e44cd8ce54d0
danilopinhomoreira <146568955+danilopinhomoreira@users.noreply.github.com>
2024-06-27 19:35:21 -0300

    Update Bebida.cs
    Retorno para a versão da Sprint 2.

--- codigo/Bebida.cs
+++ codigo/Bebida.cs
@@ -1,8 +1,15 @@
-using system;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
- public class Bebida : Produto
- {
-     public Bebida(string nome, double preco, int codigo) : base(nome, preco, codigo)
-     {
-     }
- }
+namespace Danilo_sFood
+{
+    public class Bebida : Produto
+    {
+        public Bebida(string nome, double preco, int codigo) : base(nome, preco, codigo)
+        {
+        }
+    }
+}

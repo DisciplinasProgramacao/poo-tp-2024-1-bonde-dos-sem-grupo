517a73d4c268a3d144d8ad6759a9da0de6625cc2
danilopinhomoreira <146568955+danilopinhomoreira@users.noreply.github.com>
2024-06-27 19:32:23 -0300

    Update Cardapio.cs
    Retorno para a versão da Sprint 2.

--- codigo/Cardapio.cs
+++ codigo/Cardapio.cs
@@ -1,49 +1,47 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Cardapio
+namespace Danilo_sFood
 {
-    private List<IProduto> produtos;
-    private int ultimoCodigoConsultado;
-
-    public Cardapio()
+    public class Cardapio
     {
-        produtos = new List<IProduto>
-    {
-        // Produtos do restaurante original
-        new Comida("Moqueca de Palmito", 32, GetProximoCodigo()),
-        new Comida("Falafel Assado", 20, GetProximoCodigo()),
-        new Comida("Salada Primavera com Macarrão Konjac", 25, GetProximoCodigo()),
-        new Comida("Escondidinho de Inhame", 18, GetProximoCodigo()),
-        new Comida("Strogonoff de Cogumelos", 35, GetProximoCodigo()),
-        new Comida("Caçarola de legumes", 22, GetProximoCodigo()),
-        new Bebida("Água", 3, GetProximoCodigo()),
-        new Bebida("Copo de suco", 7, GetProximoCodigo()),
-        new Bebida("Refrigerante orgânico", 7, GetProximoCodigo()),
-        new Bebida("Cerveja vegana", 9, GetProximoCodigo()),
-        new Bebida("Taça de vinho vegano", 18, GetProximoCodigo()),
+        private List<IProduto> produtos;
+        private int ultimoCodigoConsultado;
 
-        // Produtos do café OOCV
-        new CafeComida("Não de queijo", 5, GetProximoCodigo()),
-        new CafeComida("Bolinha de cogumelo", 7, GetProximoCodigo()),
-        new CafeComida("Rissole de palmito", 7, GetProximoCodigo()),
-        new CafeComida("Coxinha de carne de jaca", 8, GetProximoCodigo()),
-        new CafeComida("Fatia de queijo de caju", 9, GetProximoCodigo()),
-        new CafeComida("Biscoito amanteigado", 3, GetProximoCodigo()),
-        new CafeComida("Cheesecake de frutas vermelhas", 15, GetProximoCodigo()),
-        new CafeBebida("Água", 3, GetProximoCodigo()),
-        new CafeBebida("Copo de suco", 7, GetProximoCodigo()),
-        new CafeBebida("Café espresso orgânico", 6, GetProximoCodigo())
-    };
-    }
+        public Cardapio()
+        {
+            produtos = new List<IProduto>
+        {
+            new Comida("Moqueca de Palmito", 32, GetProximoCodigo()),
+            new Comida("Falafel Assado", 20, GetProximoCodigo()),
+            new Comida("Salada Primavera com Macarrão Konjac", 25, GetProximoCodigo()),
+            new Comida("Escondidinho de Inhame", 18, GetProximoCodigo()),
+            new Comida("Strogonoff de Cogumelos", 35, GetProximoCodigo()),
+            new Comida("Caçarola de legumes", 22, GetProximoCodigo()),
+            new Bebida("Água", 3, GetProximoCodigo()),
+            new Bebida("Copo de suco", 7, GetProximoCodigo()),
+            new Bebida("Refrigerante orgânico", 7, GetProximoCodigo()),
+            new Bebida("Cerveja vegana", 9, GetProximoCodigo()),
+            new Bebida("Taça de vinho vegano", 18, GetProximoCodigo())
+        };
+        }
 
-    public List<IProduto> ListarProdutos()
-    {
-        return produtos;
-    }
+        public List<IProduto> ListarProdutos()
+        {
+            return produtos;
+        }
 
-    private int GetProximoCodigo()
-    {
-        ultimoCodigoConsultado++;
-        return ultimoCodigoConsultado;
+        public IProduto BuscarProdutoPorCodigo(int codigo)
+        {
+            return produtos.FirstOrDefault(p => p.GetCodigo() == codigo);
+        }
+
+        private int GetProximoCodigo()
+        {
+            return ++ultimoCodigoConsultado;
+        }
     }
 }
